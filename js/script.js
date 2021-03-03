4b49d9d79dffb941437ec0ec316e92485adf1b8c
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-03-03 14:40:00 +0100

    add. scheme

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-alert("ciao");
+/*Chiedere all’utente il cognome inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’
+stampa la lista ordinata alfabeticamente scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova*/
+
+
+// 1 chiedere cognome
+
+
+// 2 inserirlo in un array
+
+// 3 stampa lista ordinata alfabeticamente
+
+// 4 stampa posizione del nuovo cognome nella lista
